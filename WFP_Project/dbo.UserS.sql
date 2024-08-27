bbfa79cf344737d7e7187ed95f6ef845e30ee4fd
AndriiBalakhtin <146086348+AndriiBalakhtin@users.noreply.github.com>
2024-08-28 01:44:47 +0200

    Secure small update

---
+++ WFP_Project/dbo.UserS.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UserS] (
+    [Id]     INT          IDENTITY (1, 1) NOT NULL,
+    [Date]   DATETIME     DEFAULT (getdate()) NOT NULL,
+    [Force]  VARCHAR (25) NULL,
+    [1st]    INT          NULL,
+    [Weight] VARCHAR (50) NULL,
+    [2nd]    INT          NULL,
+    [Goal]   VARCHAR (75) NULL,
+    [3rd]    INT          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
