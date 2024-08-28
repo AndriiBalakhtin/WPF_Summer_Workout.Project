b0e606afcea2f9e39559d6d36e14e4ce95654341
AndriiBalakhtin <146086348+AndriiBalakhtin@users.noreply.github.com>
2024-08-28 13:41:43 +0200

    Archive, better UI, and added button delete with anim

--- WFP_Project/WFP_Project/Pages/Archive.xaml.cs
+++ WFP_Project/WFP_Project/Pages/Archive.xaml.cs
@@ -2,7 +2,10 @@
 using System.Data;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 using WFP_Project.Classes;
 
 namespace WFP_Project.Pages
@@ -78,7 +81,7 @@ namespace WFP_Project.Pages
                             Margin = new Thickness(5)
                         };
 
-                        LinearGradientBrush folderButtonBrush = new LinearGradientBrush
+                        LinearGradientBrush gradientBrush = new LinearGradientBrush
                         {
                             StartPoint = new Point(0, 0),
                             EndPoint = new Point(1, 1),
@@ -89,87 +92,113 @@ namespace WFP_Project.Pages
                     }
                         };
 
-                        Button folderButton = new Button
+                        Border roundedBorder = new Border
                         {
-                            Width = 120,
+                            Width = 174,
                             Height = 40,
-                            Margin = new Thickness(10),
-                            FontSize = 16,
-                            FontWeight = FontWeights.ExtraBold,
-                            HorizontalContentAlignment = HorizontalAlignment.Center,
-                            VerticalContentAlignment = VerticalAlignment.Center,
-                            Background = folderButtonBrush,
-                            Content = new StackPanel
-                            {
-                                Orientation = Orientation.Horizontal,
-                                Children =
-                        {
-                            new PackIcon
-                            {
-                                Kind = PackIconKind.Folder,
-                                Width = 20,
-                                Height = 20,
-                                Margin = new Thickness(0, 0, 5, 0)
-                            },
-                            new TextBlock
-                            {
-                                Text = tableName,
-                                VerticalAlignment = VerticalAlignment.Center
-                            }
-                        }
-                            }
+                            CornerRadius = new CornerRadius(10),
+                            Background = gradientBrush,
+                            Padding = new Thickness(10),
+                            Margin = new Thickness(5),
+                            Cursor = Cursors.Hand
                         };
 
-                        folderButton.Style = (Style)this.FindResource("RoundedButtonStyle");
-                        folderButton.Click += (s, e) => LoadTableData(tableName);
-
-                        LinearGradientBrush deleteButtonBrush = new LinearGradientBrush
+                        TextBlock textBlock = new TextBlock
                         {
-                            StartPoint = new Point(0, 0),
-                            EndPoint = new Point(1, 1),
-                            GradientStops = new GradientStopCollection
-                    {
-                        new GradientStop(Colors.Aquamarine, 0),
-                        new GradientStop(Colors.Azure, 1)
-                    }
+                            Text = tableName,
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center,
+                            FontSize = 16,
+                            FontWeight = FontWeights.Bold,
+                            Foreground = Brushes.Black
+                        };
+
+                        roundedBorder.Child = textBlock;
+
+                        roundedBorder.MouseEnter += (s, e) =>
+                        {
+                            roundedBorder.Background = new LinearGradientBrush
+                            {
+                                StartPoint = new Point(0, 0),
+                                EndPoint = new Point(1, 1),
+                                GradientStops = new GradientStopCollection
+                        {
+                            new GradientStop(Colors.LightBlue, 0),
+                            new GradientStop(Colors.White, 1)
+                        }
+                            };
+                        };
+
+                        roundedBorder.MouseLeave += (s, e) =>
+                        {
+                            roundedBorder.Background = gradientBrush;
+                        };
+
+                        roundedBorder.MouseLeftButtonUp += (s, e) =>
+                        {
+                            LoadTableData(tableName);
+                        };
+
+                        Grid grid = new Grid
+                        {
+                            Width = 50,
+                            Height = 50
+                        };
+
+                        Ellipse ellipse = new Ellipse
+                        {
+                            Width = 40,
+                            Height = 40,
+                            Fill = Brushes.LightGray,
+                            Opacity = 0,
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            VerticalAlignment = VerticalAlignment.Center
                         };
 
                         Button deleteButton = new Button
                         {
-                            Width = 100,
-                            Height = 40,
-                            Margin = new Thickness(10),
-                            FontSize = 14,
-                            FontWeight = FontWeights.ExtraBold,
-                            HorizontalContentAlignment = HorizontalAlignment.Center,
-                            VerticalContentAlignment = VerticalAlignment.Center,
-                            Background = deleteButtonBrush,
-                            Content = new StackPanel
-                            {
-                                Orientation = Orientation.Horizontal,
-                                Children =
-                        {
-                            new PackIcon
+                            Width = 30,
+                            Height = 30,
+                            Background = Brushes.Transparent,
+                            BorderBrush = Brushes.Transparent,
+                            Content = new PackIcon
                             {
                                 Kind = PackIconKind.Delete,
                                 Width = 20,
                                 Height = 20,
-                                Margin = new Thickness(0, 0, 5, 0)
-                            },
-                            new TextBlock
-                            {
-                                Text = "Delete",
-                                VerticalAlignment = VerticalAlignment.Center
-                            }
-                        }
+                                Foreground = Brushes.Red
                             }
                         };
 
-                        deleteButton.Style = (Style)this.FindResource("RoundedButtonStyle");
                         deleteButton.Click += (s, e) => DeleteArchivedTable(tableName);
 
-                        stackPanel.Children.Add(folderButton);
-                        stackPanel.Children.Add(deleteButton);
+                        deleteButton.MouseEnter += (s, e) =>
+                        {
+                            DoubleAnimation fadeIn = new DoubleAnimation
+                            {
+                                From = 0,
+                                To = 0.5,
+                                Duration = TimeSpan.FromSeconds(0.3)
+                            };
+                            ellipse.BeginAnimation(UIElement.OpacityProperty, fadeIn);
+                        };
+
+                        deleteButton.MouseLeave += (s, e) =>
+                        {
+                            DoubleAnimation fadeOut = new DoubleAnimation
+                            {
+                                From = 0.5,
+                                To = 0,
+                                Duration = TimeSpan.FromSeconds(0.3)
+                            };
+                            ellipse.BeginAnimation(UIElement.OpacityProperty, fadeOut);
+                        };
+
+                        grid.Children.Add(ellipse);
+                        grid.Children.Add(deleteButton);
+
+                        stackPanel.Children.Add(roundedBorder);
+                        stackPanel.Children.Add(grid);
 
                         tablesWrapPanel.Children.Add(stackPanel);
                     }
